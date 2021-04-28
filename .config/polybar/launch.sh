862f254be034a6097782462d68e312c9740b00ed
The_philosopher <shinudonney@tuta.io>
2021-04-28 20:44:19 +0530

    Add new polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Terminate already running bar instances
-killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
+pkill polybar
+polybar -r top &
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
-
-echo "Bars launched..."
+if [[ $(xrandr -q | grep 'HDMI-1 connected') ]]; then
+    polybar -r top_external &
+fi
