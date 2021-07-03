c7ac313411e42874a4fe792ce5cb439ed253ca82
The_philosopher <shinudonney@tuta.io>
2021-07-03 20:31:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,6 +32,6 @@ setupstow
 
 # list of programs you want to install modify it as necessary, and test it out
 # and remove the n flag when you are ready to install the dot files 
-stow -nv bash dev fonts misc scripts shell wm x11 zsh
+stow -nv bash dev fonts misc scripts shell wallie wm x11 zsh
 
 cleanup 0
